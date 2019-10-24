b84431e3b6ca765cb7e1b0619c94ba48144c0dc5
belyakovaas <belyakova_as@mail.ru>
2019-10-24 11:28:05 +0300

    тесты для компонента Script разбиты на два: для тестирования рендеринга и для тестирования реакций на клики

--- src/ui/components/ScriptList/Script/__tests__/Script.test.tsx
+++ src/ui/components/ScriptList/Script/__tests__/Script.test.tsx
@@ -2,20 +2,33 @@ import React from "react";
 import { render, cleanup, fireEvent } from "@testing-library/react";
 import ProviderWithComponent from "../../../../store/ProviderWithComponent";
 import Script from "../Script";
-import {ScriptValue} from "../../../../util/indexedDB";
+import { ScriptValue } from "../../../../util/indexedDB";
 
 describe("Script", () => {
     beforeEach(cleanup);
-
+    const testTour: ScriptValue = {
+        key: "custom-key",
+        name: "custom name",
+        code: "...",
+        desc: "custom description",
+        date: "Thu Oct 24 2019 10:52:15 GMT+0300 (Москва, стандартное время)"
+    };
+    const onClick = jest.fn();
     it("should Script render", () => {
-        const testTour: ScriptValue = {
-            key: "custom-key",
-            name: "custom name",
-            code: "...",
-            desc: "custom description",
-            date: "Thu Oct 24 2019 10:52:15 GMT+0300 (Москва, стандартное время)"
-        };
         const onClick = jest.fn();
+        const { getByTestId } = render(
+            ProviderWithComponent(() => <Script onClick={onClick} tour={testTour} />, {
+                SelectedTourState: {
+                    tourDB: {
+                        key: "test-key"
+                    }
+                }
+            } as any)()
+        );
+        expect(getByTestId("tour-name").textContent).toBe("custom name");
+        expect(getByTestId("tour-time").textContent).toBe("24-10-2019 в 10:52");
+    });
+    it("should Script onClick", () => {
         const { getByTestId, queryByText } = render(
             ProviderWithComponent(() => <Script onClick={onClick} tour={testTour} />, {
                 SelectedTourState: {
@@ -25,9 +38,6 @@ describe("Script", () => {
                 }
             } as any)()
         );
-
-        expect(getByTestId("tour-name").textContent).toBe("custom name");
-        expect(getByTestId("tour-time").textContent).toBe("24-10-2019 в 10:52");
         expect(queryByText("Редактирование шаблона")).toBeNull();
         fireEvent.click(getByTestId("edit-button"));
         expect(queryByText("Редактирование шаблона")).not.toBeNull();
